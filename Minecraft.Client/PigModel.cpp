38d6a709978ea592e3977a5961cae14d4caefc0d
Teski <diegod31026@gmail.com>
2026-04-01 08:01:25 -0500

    pig have flat snout

--- Minecraft.Client/PigModel.cpp
+++ Minecraft.Client/PigModel.cpp
@@ -4,7 +4,6 @@
 
 PigModel::PigModel() : QuadrupedModel(6, 0)
 {
-	head->texOffs(16, 16)->addBox(-2.0f, 0.0f, -9.0f, 4, 3, 1, 0.0f);
 	yHeadOffs = 4;
 
 	head->compile(1.0f/16.0f);
@@ -12,7 +11,6 @@ PigModel::PigModel() : QuadrupedModel(6, 0)
 
 PigModel::PigModel(float grow) : QuadrupedModel(6, grow)
 {
-	head->texOffs(16, 16)->addBox(-2.0f, 0.0f, -9.0f, 4, 3, 1, grow);
 	yHeadOffs = 4;
 
 	head->compile(1.0f/16.0f);
